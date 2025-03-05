a8408692e61032d4852301d24eeac253559c484f
sabbir72 <sabbircse72@gmail.com>
2025-03-05 13:01:20 +0600

    Aliasing

--- cypress/e2e/test3.cy.js
+++ cypress/e2e/test3.cy.js
@@ -14,5 +14,24 @@ describe(" ", () => {
     });
 
     cy.get(".connectors-its-ul>li").its("length");
+
+    const arr = ['foo', 'bar', 'baz']
+
+cy.wrap(arr).spread(function(foo, bar, baz){
+  expect(foo).to.eq('foo')
+  expect(bar).to.eq('bar')
+  expect(baz).to.eq('baz')
+
+  cy.get(".dropdown-toggle").should("be.visible").click();
+    cy.wait(1000);
+  cy.get("ul.dropdown-menu").contains("Aliasing").should("be.visible").click();
+
+  cy.xpath("//button[normalize-space()='Get Comment']").click();
+
+  cy.get('.as-table')
+  .find('tbody>tr').first()
+  .find('td').first()
+  .find('button').as('firstBtn')
+})
   });
 });
