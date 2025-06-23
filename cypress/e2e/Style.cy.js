73a32b048e26f6e1c44dc3831c6081bb62e8e2c5
sabbir72 <sabbircse72@gmail.com>
2025-06-23 17:30:15 +0600

    style doc

---
+++ cypress/e2e/Style.cy.js
@@ -0,0 +1,50 @@
+describe('Login Test', () => {
+  it('Visits login page and logs in', () => {
+    cy.visit('https://stagev15.inctl.net/')
+
+   cy.get('input#login_email').type('Administrator')
+
+    cy.get('input#login_password').type('admin')
+
+    cy.contains('button', 'Login').click()
+
+    cy.wait(1000)
+
+    cy.get('input#navbar-search').type('Style')
+
+    cy.wait(1000)
+
+   cy.get('li[aria-selected="true"]').first().click()
+
+ cy.get('button[data-label="Add Style"]').click()
+
+   cy.get('select[data-fieldname="type"]').select('Set Product')
+
+//    uniq name make
+    const uniqueName = 'style_' + Math.random().toString(10).substring(2,4) + '_' + Date.now()
+
+   cy.get('input[data-fieldname="style_name"]').type(uniqueName)
+   cy.wait(3000)
+   cy.get('input[data-fieldname="style_no"]').type(uniqueName)
+
+    cy.get('input[data-fieldname="item_type"]').click().type('Jacket')
+
+    cy.get('input[data-fieldname="customer"]').click().type('JACK & JONES')
+
+    cy.get('input[data-fieldname="item_department"]').click().type("Men's Wear")
+
+     cy.wait(3000)
+
+cy.get('button.grid-add-row').eq(1).click()
+// cy.get('div[data-fieldname="combinations"] button').eq(2)
+
+
+
+
+
+
+ 
+
+    
+  })
+})
