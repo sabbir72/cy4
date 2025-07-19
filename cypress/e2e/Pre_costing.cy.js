463476740bdbd738d90d18a2c2d1792f2e9afdfb
new <sabbirsce72@gmail.com>
2025-07-19 17:22:50 +0600

    precost3red tab

--- cypress/e2e/Pre_costing.cy.js
+++ cypress/e2e/Pre_costing.cy.js
@@ -10,7 +10,7 @@ describe("Login and PreCosting Single Product", () => {
 
     // Click Add Pre Costing
     cy.get('button[data-label="Add Pre Costing"]').click();
-    cy.wait(2000);
+    cy.wait(1000);
 
     // Select Finished Goods Type
     cy.scrollTo("top");
@@ -33,9 +33,8 @@ describe("Login and PreCosting Single Product", () => {
     cy.wait(1000);
     // selection
 
-  //  cy.get('select[data-fieldname="rm_cost_as_per"]').select("Manual");
-  //  cy.wait(1000)
-
+    //  cy.get('select[data-fieldname="rm_cost_as_per"]').select("Manual");
+    //  cy.wait(1000)
 
     // Set Currency
     // cy.get('input[data-fieldname="currency"]').click().clear().type("USD");
@@ -44,11 +43,10 @@ describe("Login and PreCosting Single Product", () => {
 
     // Custom Command: Add Fabric Row
     Cypress.Commands.add("addFabricRow", () => {
-     cy.contains("label", "Fabric Items") // টেবিলের লেবেল খুঁজে পাবে
-       .parents(".form-group") // পুরো গ্রুপটা ধরবে
-       .find("button.grid-add-row") // সেই গ্রুপের মধ্যে add row
-       .click();
-
+      cy.contains("label", "Fabric Items") // টেবিলের লেবেল খুঁজে পাবে
+        .parents(".form-group") // পুরো গ্রুপটা ধরবে
+        .find("button.grid-add-row") // সেই গ্রুপের মধ্যে add row
+        .click();
     });
 
     // Add and Edit Fabric Row
@@ -65,28 +63,25 @@ describe("Login and PreCosting Single Product", () => {
     cy.get('div[role="option"]').contains("60/40 CVC FLEECE").click();
     cy.wait(1000);
 
-    cy.get('input[data-fieldname="dyeing_or_fabric_color"]')
-      .first()
-      .click()
-      .type("Black");
-    cy.get('ul[role="listbox"] div[role="option"]').contains("Black").click();
+    // cy.get('input[data-fieldname="dyeing_or_fabric_color"]')
+    //   .first()
+    //   .click()
+    //   .type("Black");
+    // cy.get('ul[role="listbox"] div[role="option"]').contains("Black").click();
+    // cy.wait(1000);
+
+    // cy.get('input[data-fieldname="fabric_gsm"]').click().type("GSM-130-0539");
+    // cy.get('div[role="option"]').contains("GSM-130-0539").click();
+    // cy.wait(1000);
+
+    // cy.get('input[data-fieldname="finish_dia_width"]')
+    //   .click()
+    //   .type("DIA-00001");
+    // cy.get('div[role="option"]').contains("DIA-00001").click();
     cy.wait(1000);
 
-    cy.get('input[data-fieldname="fabric_gsm"]').click().type("GSM-130-0539");
-    cy.get('div[role="option"]').contains("GSM-130-0539").click();
-    cy.wait(1000);
-
-    cy.get('input[data-fieldname="finish_dia_width"]')
-      .click()
-      .type("DIA-00001");
-    cy.get('div[role="option"]').contains("DIA-00001").click();
-    cy.wait(1000);
-
-    cy.get(
-      "div:nth-child(14)>div:nth-child(1)>div:nth-child(2)>div:nth-child(1)>div:nth-child(1)>div:nth-child(1)>input"
-    ).click();
-    cy.findByRole("option", { name: "Local" }).click();
-    cy.wait(1000);
+     cy.get('input[data-fieldname="source"]').eq(0).click({force:true}).type("local")
+     cy.get('div[role="option"]').contains("Local").click();
 
     cy.get('div[data-fieldname="quantity_and_rate"] input')
       .first()
@@ -108,94 +103,56 @@ describe("Login and PreCosting Single Product", () => {
     cy.wait(1000);
 
     // ================= Accessories Section ====================
-    cy.contains("label", "Accessories Items").scrollIntoView();
-    cy.wait(2000);
+    // cy.contains("label", "Accessories Items").scrollIntoView();
+    // cy.wait(2000);
 
-    Cypress.Commands.add("addAccRow", () => {
-      cy.contains("label", "Accessories Items") 
-        .parents(".form-group") 
-        .find("button.grid-add-row") 
-        .click();
-    });
+    // Cypress.Commands.add("addAccRow", () => {
+    //   cy.contains("label", "Accessories Items")
+    //     .parents(".form-group")
+    //     .find("button.grid-add-row")
+    //     .click();
+    // });
 
-   cy.addAccRow();
+    // cy.addAccRow();
 
-    cy.get('div.btn-open-row[data-original-title="Edit"]').eq(1).click();
+    // cy.get('div.btn-open-row[data-original-title="Edit"]').eq(1).click();
 
-    cy.wait(1000);
-
-   Cypress.Commands.add("Acce_MaterialRow", (data) => {
-     // Fill Material Code (item_code field, 2nd occurrence)
-     cy.get('input[data-fieldname="item_code"]')
-       .eq(1)
-       .click({ force: true })
-       .clear()
-       .type(data.material_code);
-
-     // Fill Source (Link Field, 2nd occurrence)
-     cy.get('input[data-fieldname="source"]')
-       .eq(1)
-       .click({ force: true })
-       .clear()
-       .type(data.source)
-       .wait(200); // wait a bit for dropdown to appear
-
-     // Wait for dropdown and select source
-     cy.get('ul[role="listbox"] li')
-       .contains(data.source).first()
-       .click();
-
-     // Fill Qty Ratio
-     cy.get('input[data-fieldname="qty_ratio"]')
-       .eq(1)
-       .click({ force: true })
-       .clear()
-       .type(data.Qty_ratio);
-
-     // Fill User Rate Entry
-     cy.get('input[data-fieldname="user_rate_entry"]')
-       .eq(1)
-       .click({ force: true })
-       .clear()
-       .type(data.rate);
-   });
-
-  
-
-   cy.Acce_MaterialRow({
-     material_code: "ACC-456",
-     source: "Import",    // বা Local / In House
-     Qty_ratio: "1.25",
-     rate: "12.50",
-   });
-
-
-
-    // cy.get('input[data-fieldname="item_code"]')
-    //   .first()
-    //   .clear()
-    //   .type("Accessories-0022");
-    // cy.get('div[role="option"]').contains("Accessories Test").first().click();
     // cy.wait(1000);
 
-    // cy.get('div[data-fieldname="source"] input').first().click().type("Local");
-    // cy.get('div[role="option"]').contains("Local").click({ force: true });
-    // cy.wait(1000);
+    //    cy.get('input[data-fieldname="item_code"]').eq(1).click({ force: true }).clear().type("Accessories-0003")
+     
+    // cy.get('div[data-fieldname="source"] input').eq(1).click()
+    //  cy.get('div[role="option"]').contains("Local").eq(0).click();
 
-    // cy.get('div[data-fieldname="quantity_and_rate"] input')
-    //   .first()
-    //   .scrollIntoView()
-    //   .clear()
-    //   .type("2.5");
-    // cy.wait(1000);
+    // // consumtion
+    // cy.get('input[data-fieldname="qty_ratio"]').eq(1).click().clear().type(0.554)
 
-    // cy.get('input[data-fieldname="user_rate_entry"]')
-    //   .first()
-    //   .click()
-    //   .type("4.25");
-    // cy.wait(1000);
+    // cy.get('input[data-fieldname="user_rate_entry"]').eq(1).click().clear().type(1.2)
+    // cy.get('button.grid-collapse-row').eq(1).click({ force: true });
 
-    // cy.contains("kbd", "Ctrl + Down").click({ force: true });
+// Accesories template
+
+cy.get('input[data-fieldname="accessories_template"]').click().type("TOM TAILOR-00915")
+cy.get('div[role="option"] p[title="TOM TAILOR-00915"]').click()
+
+
+
+
+// pre_costing_data_tab
+cy.get('a[data-fieldname="pre_costing_data_tab"]').click()
+cy.get('input[data-fieldname="payment_terms"]').click().type("120 Days")
+cy.get('div[role="option"]').contains("120 Days").click({force:true});
+
+
+cy.get('input[data-fieldname="shipping_mode"]').click().type("Sea")
+cy.get('div[role="option"]').contains("Sea").click({force:true});
+
+cy.get('input[data-fieldname="delivery_terms"]').click().type("FOB")
+cy.get('div[role="option"]').contains("FOB").click({force:true});
+
+
+cy.get('input[data-fieldname="sales_price"]').clear().click().type(2.00)
+
     cy.wait(1000);
   });
 });
